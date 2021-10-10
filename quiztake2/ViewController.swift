2ec4508cb37b7d157c27076a170bd280166102d4
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2021-10-10 07:18:32 -0600

    Initial Commit

---
+++ quiztake2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quiztake2
+//
+//  Created by James Young on 10/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
